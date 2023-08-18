21bdeaac30cfadb6a74351cbfe359c1cedea33a7
EC2 Default User <ec2-user@ip-172-31-37-223.ap-south-1.compute.internal>
2023-08-18 17:21:30 +0000

    changes done to service_to_monitor

--- scripts/service_to_monitor.sh
+++ scripts/service_to_monitor.sh
@@ -6,6 +6,6 @@ do
    ps -C $i
    if [ $? -ne 0 ]
    then
-	   echo "subject: $i service is not running "| sendmail -v sagarshastry42@gmail.com
+	   echo "the $i service is stopped alert" | mail -s "$i service is not running" sagarshastry42@gmail.com
    fi
 done
