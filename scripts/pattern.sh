9f8526214baac04125736980cdbb252ffeef2d89
EC2 Default User <ec2-user@ip-172-31-35-16.ap-south-1.compute.internal>
2023-08-20 03:03:19 +0000

    adding pattern.sh

---
+++ scripts/pattern.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "enter the pattern which you need to find"
+read pattern
+grep -R -i -l "$pattern" > output
+
+if [ $? -eq 0 ]
+then
+	echo " the below files contain pattern $pattern "
+	cat output
+else
+	echo " the file doesn't contain pattern $pattern "
+fi
